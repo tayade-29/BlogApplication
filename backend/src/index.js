98954bcbc4e5002f3d27858e46ffbb8b4a0f23df
tayade-29 <tayadepranjal2903@gmail.com>
2025-01-05 01:21:14 +0530

    url updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,10 +34,10 @@ app.get('/', (req, res) => {
 
 // Serve React build folder (for full-stack deployment)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.use(express.static(path.join(__dirname, './frontend/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
   });
 }
 
